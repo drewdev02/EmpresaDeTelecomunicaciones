ed87bce93f58f5e8e2da86feb2c2440e22e08de0
voidt <voidtracer6@gmail.com>
2023-01-17 00:44:45 -0500

    Def de llamada internacional

--- src/main/java/Entidades/LlamandaInternacional.java
+++ src/main/java/Entidades/LlamandaInternacional.java
@@ -1,4 +1,20 @@
 package Entidades;
 
-public class LlamandaInternacional {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+
+public class LlamandaInternacional extends Llamada{
+    float tarifaPais;
+
+    public LlamandaInternacional (float minutosDuracion, String numeroOrigen, String numeroDestino, LocalDateTime fechaHora,
+                                  float costoLlamada, float tarifaPais) {
+        super(minutosDuracion,numeroOrigen,numeroDestino,fechaHora,costoLlamada);
+
+        this.tarifaPais = tarifaPais;
+    }
 }
