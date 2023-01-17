b55dc8d65da898be762841af4a8bdb523c9c8194
Andrey Rodriguez <andrey.rgz.2016@gmail.com>
2023-01-17 00:34:51 -0500

    MOD

--- src/main/java/Entidades/LlamandaInternacional.java
+++ src/main/java/Entidades/LlamandaInternacional.java
@@ -1,4 +1,19 @@
 package Entidades;
 
-public class LlamandaInternacional {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LlamandaInternacional extends Cliente {
+    private float tarifaPais;
+
+    public LlamandaInternacional(String nombre, String numeroCarnet, String direccion, String telefono, int numeroLlamadas, String estado, String horaDespertar, double pagoMensual, float tarifaPais) {
+        super(nombre, numeroCarnet, direccion, telefono, numeroLlamadas, estado, horaDespertar, pagoMensual);
+        this.tarifaPais = tarifaPais;
+    }
+
+    public LlamandaInternacional() {
+        super();
+    }
 }
