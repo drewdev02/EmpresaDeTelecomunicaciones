16907a21597163da083ac99926fbdef113ef357b
Andrey Rodriguez <andrey.rgz.2016@gmail.com>
2023-01-17 01:02:31 -0500

    Modificacion

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,24 +1,30 @@
 import Entidades.CentralTelefonica;
 import Entidades.Cliente;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class MainTest {
+    CentralTelefonica centralTelefonica;
+    Cliente cliente;
+
+    @Before
+    public void setUp() {
+        CentralTelefonica centralTelefonica = new CentralTelefonica();
+        Cliente cliente = new Cliente("Juan", "123456", "Calle 1", "123456", 10, "Activo", "8:00", 100);
+    }
+
     @Test
     public void testAddCliente() {
-        CentralTelefonica centralTelefonica = new CentralTelefonica();
-        Cliente cliente = new Cliente();
         centralTelefonica.addCliente(cliente);
+
         assertTrue(centralTelefonica.getClientes().contains(cliente));
     }
 
     @Test
     public void testRmdCliente() {
-        CentralTelefonica centralTelefonica = new CentralTelefonica();
-        Cliente cliente = new Cliente();
-        centralTelefonica.addCliente(cliente);
         centralTelefonica.remCliente(cliente);
         assertFalse(centralTelefonica.getClientes().contains(cliente));
     }
