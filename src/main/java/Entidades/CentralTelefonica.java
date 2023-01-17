13704732a605b2a2deb44d96f6d7b0f536be3293
Andrey Rodriguez <andrey.rgz.2016@gmail.com>
2023-01-17 00:13:35 -0500

    MOD

--- src/main/java/Entidades/CentralTelefonica.java
+++ src/main/java/Entidades/CentralTelefonica.java
@@ -6,6 +6,12 @@ public class CentralTelefonica {
     private ArrayList<Cliente> clientes = new ArrayList<>();
 
     public void addCliente(Cliente cliente) {
+        this.clientes.add(cliente);
+
+    }
+
+    public void remCliente(Cliente cliente) {
+        this.clientes.add(cliente);
 
     }
 }
