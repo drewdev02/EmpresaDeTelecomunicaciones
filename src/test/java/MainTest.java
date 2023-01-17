e79972331d8d0a0a6bc444f6d54fa948d4e80a8c
Andrey Rodriguez <andrey.rgz.2016@gmail.com>
2023-01-17 00:56:49 -0500

    Agregado testRmdCliente

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,6 +2,7 @@ import Entidades.CentralTelefonica;
 import Entidades.Cliente;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class MainTest {
@@ -11,6 +12,15 @@ public class MainTest {
         Cliente cliente = new Cliente();
         centralTelefonica.addCliente(cliente);
         assertTrue(centralTelefonica.getClientes().contains(cliente));
+    }
+
+    @Test
+    public void testRmdCliente() {
+        CentralTelefonica centralTelefonica = new CentralTelefonica();
+        Cliente cliente = new Cliente();
+        centralTelefonica.addCliente(cliente);
+        centralTelefonica.remCliente(cliente);
+        assertFalse(centralTelefonica.getClientes().contains(cliente));
     }
 
 }
