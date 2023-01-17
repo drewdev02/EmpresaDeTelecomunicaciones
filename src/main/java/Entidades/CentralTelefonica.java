e9b73f9d5263c7311e207cf95797674623e9648f
Andrey Rodriguez <andrey.rgz.2016@gmail.com>
2023-01-17 00:49:45 -0500

    Agregado notaciones Getter y Setter

--- src/main/java/Entidades/CentralTelefonica.java
+++ src/main/java/Entidades/CentralTelefonica.java
@@ -1,7 +1,12 @@
 package Entidades;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 
+@Getter
+@Setter
 public class CentralTelefonica {
     private ArrayList<Cliente> clientes = new ArrayList<>();
 
